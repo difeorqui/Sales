68b5d856c9958cb6a3b31d5b86c017c219923578
difeorqui <43541893+difeorqui@users.noreply.github.com>
2018-12-16 17:25:32 -0500

    Clase 2

---
+++ Sales.Backend/Models/LoalDataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Sales.Backend.Models
+{
+  using Domain.Models;
+
+  public class LoalDataContext : DataContext
+  {
+    public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+  }
+}
